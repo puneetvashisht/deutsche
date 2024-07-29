5cf2efb52a5bf1c8f6230accffe85a5160b2274f
Puneet Vashisht <puneetvashisht@gmail.com>
2024-07-29 12:25:46 +0530

    test

--- frontend/react/my-first-app/src/Badge.js
+++ frontend/react/my-first-app/src/Badge.js
@@ -6,6 +6,7 @@ const Badge = (props) => {
 
     const incrementCount = () => {
         setCount(count + 1);
+        // props.caption = 3
     }
 
     return (
--- frontend/react/my-second-app/src/components/Dropdown.js
+++ frontend/react/my-second-app/src/components/Dropdown.js
@@ -1,4 +1,4 @@
-import React, { useState } from'react';
+import React, { useEffect, useRef, useState } from'react';
 import DropdownItem from './DropdownItem';
 const Dropdown = () => {
 
@@ -6,18 +6,35 @@ const Dropdown = () => {
     const [cuisines, setCuisines] = useState(['Indian', 'Mexican', 'Italian', 'American', 'Chinese']);
     const [caption, setCaption] = useState('Select a cuisine');
 
+    const dropdownRef = useRef(123);
+
+    // useEffect(() => {
+    //     console.log('Initial render or update');
+    //     // fetch('https://example.com/api/cuisines/'+caption)
+
+    // }, [caption])
+    
+
     const handleItemClick = (text) => {
         console.log(text +'selected in the parent'  );
         setCaption(text);
         setIsOpen(false);
     };
 
-    let cuisineList = cuisines.map((cuisine, index) => (
+    const captureValue = () => {
+        console.log(dropdownRef.current.value);
+    } 
+
+    let cuisineList = cuisines && cuisines.map((cuisine, index) => (
         <DropdownItem key={index} text={cuisine} whenItemClicked={handleItemClick}></DropdownItem>
     ));
 
     return (
         <div className="dropdown">
+            <input ref={dropdownRef}  type="text" className="form-control" placeholder="Search for cuisine"/>
+            <button onClick={captureValue}>Test</button>
+             <pre>{dropdownRef.current}</pre>
+           
             <button onClick={()=> setIsOpen(!isOpen)} className="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
                 {caption}
             </button>
